23fc912f8f74359a3845b888798c7db0f3801016
UnixCF <41532859+unixcf@users.noreply.github.com>
2019-01-04 20:50:41 -0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,61 @@
+var tmi = require('tmi.js');
+var readline = require('readline');
+var https = require('https');
+
+var canais = [];
+var client = null;
+
+
+
+executar_canais();
+conectar();
+
+setInterval(function(){
+
+    canais = [];
+
+    client.disconnect();
+
+    executar_canais();
+    conectar();
+
+}, 30 * 60 * 1000);
+
+function executar_canais() {
+    var site = https.get("https://raw.githubusercontent.com/unixcf/twitchCanais-UnixChat/master/canais%2Ctxt", function(pagina) {
+    var github = readline.createInterface({
+      input: pagina
+    });
+    github.on('line', (line) => {
+      canais.push(line);
+    }).on('close', () => {
+      console.log(canais);
+    });
+  });
+  }
+
+function conectar() {
+    var options = {
+        options: {
+            debug: true
+        },
+        connection: {
+            cluster: "aws",
+            reconnect: true
+        }, 
+        identity: {
+            username: "twitch_conta",
+            password: "oauth"
+        },
+        channels: canais
+    };
+    
+    client = new tmi.client(options);
+    client.connect();
+    
+    client.on("connected", function (address, port) {
+        console.log("Conectado...");
+    });
+    
+}
+
